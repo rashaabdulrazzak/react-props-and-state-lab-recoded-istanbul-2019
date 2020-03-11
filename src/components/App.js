ee70d891e6e414899485a23bea347770ccf8130e
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-11 07:14:11 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,9 @@ class App extends React.Component {
       }
     }
   }
-
+onChangeType=()=>{
+  
+}
   render() {
     return (
       <div className="ui container">
@@ -24,7 +26,7 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} />
             </div>
             <div className="twelve wide column">
               <PetBrowser />
